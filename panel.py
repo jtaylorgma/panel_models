03b61501961e198fb25088d3878676950958a348
Josh Taylor <jtaylor@gma-us.com>
2015-06-26 15:24:09 -0400

    I finished the estimation of the models, now all that's left is printing out the summary of the models.

--- panel.py
+++ panel.py
@@ -2,7 +2,7 @@
 #Program: panel.py
 #Project: Panel Models
 #Author: Josh Taylor
-#Last Edited: 6/25/15
+#Last Edited: 6/26/15
 ########################################################################################################################
 
 """ Estimate panel data models
@@ -20,11 +20,11 @@
 """
 
 
-import statsmodels.base.model as base
-import statsmodels.base.wrapper as wrap
 import statsmodels.formula.api as sm
 import pandas as pd
 import numpy as np
+import scipy as sp
+from math import sqrt
 import re
 
 
@@ -53,6 +53,7 @@ class PanelModel:
         if type(data) is not pd.core.panel.Panel:
             raise ValueError("Data must be a panel dataframe, i.e. of type pandas.core.panel.Panel")
         self.panel = data
+        self.pooledDF = self.panel.to_frame()
         self.balanced = True
         self.timeVals = self.panel.minor_axis
         self.idVals = self.panel.major_axis
@@ -75,6 +76,13 @@ class PanelModel:
         self.cov_params = None
         self.cov_params_robust = None
         self.omega = None
+        self.tvalues = None
+        self.conf_int = None
+        self.resid = None
+        self.fittedvalues = None
+        self.rsquared = None
+        self.ssr = None
+        self.sst = None
         #TODO: RE unbalanced
 
         #TODO: Rethink how you want the fixed effects defined
@@ -116,6 +124,15 @@ class PanelModel:
         self.feParamsPValues = FE.pvalues.drop(self.indepVars)
         self.pvalues = FE.pvalues.get(self.indepVars)
         self.cov_params = FE.cov_params
+        self.tvalues = FE.tvalues
+        self.conf_int = FE.conf_int()
+        self.resid = FE.resid
+        self.fittedvalues = FE.fittedvalues
+        self.mse = FE.mse_resid
+        self.ssr = FE.ssr
+        self.rsquared = FE.rsquared
+
+
 
         if self.robust:
             pooledDf['resid'] = FE.resid
@@ -139,18 +156,25 @@ class PanelModel:
 
             X = np.matrix(XDemeaned.as_matrix())
             self.cov_params_robust = (X.T * X).inv * sumXDemeaned * (X.T * X).inv
-            #TODO: update the pvalues and se
+            self.bse = pd.Series()
+            for i in range(len(self.indepVars)):
+                self.bse[self.indepVars[i]] = sqrt(self.cov_params_robust[i,i])
+            self.tvalues = self.params / self.bse
+            _df = FE.nobs - FE.df_model - 1
+            _dfVec = np.ones(len(self.indepVars)) * _df
+            self.pvalues = sp.stats.tvalues.sf(self.tvalues, _dfVec)*2
+            _ciLowerBound = self.params - 1.96*self.bse
+            _ciUpperBound = self.params + 1.96*self.bse
+            self.conf_int = pd.merge(_ciLowerBound.to_frame(), _ciUpperBound.to_frame(), left_index=True, right_index=True)
 
 
 
 
-
-        #return FE
 
     def betasREBalanced(self):
         #first estimate the pooled model and obtain the residuals
-        pooledDF = self.panel.to_frame().reset_index()
-        pooledModel = sm.ols(formula = self.formula, data = pooledDF).fit()
+        self.pooledDF = self.panel.to_frame().reset_index()
+        pooledModel = sm.ols(formula = self.formula, data = self.pooledDF).fit()
         sigmaSqV = pooledModel.ssr/pooledModel.df_resid
         sigmaSqC = 0
         tTime = self.timeVals[:-1]
@@ -161,7 +185,7 @@ class PanelModel:
                     if t == s:
                         continue
                     sigmaSqC += pooledModel.resid[id].loc[t]*pooledModel.resid[id].loc[s]
-        sigmaSqC /=(((pooledDF.shape[0]*(len(self.timeVals) - 1))/2) - pooledModel.df_model)
+        sigmaSqC /=(((self.pooledDF.shape[0]*(len(self.timeVals) - 1))/2) - pooledModel.df_model)
         sigmaSqU = sigmaSqV - sigmaSqC
         tempSigmaSqC = np.matrix(np.ones((len(self.timeVals), len(self.timeVals)))*sigmaSqC)
         tempSigmaSqU = np.matrix(np.identity(len(self.timeVals))*sigmaSqU)
@@ -173,23 +197,76 @@ class PanelModel:
             tempY = np.matrix(self.panel.major_xs(id)[self.depVar].as_matrix())
             tempA += tempData.T * self.omega.inv * tempData
             tempXOY += tempData.T * self.omega.inv * tempY
-        self.betas = tempA.inv * tempXOY
-        self.cov_params = tempA.inv/len(self.idVals)
+
+        _params = tempA.inv * tempXOY
+        self.params = pd.Series(_params, self.indepVars)
+        _cov_params = tempA.inv/len(self.idVals)
+        self.cov_params = pd.DataFrame(_cov_params, self.indepVars, self.indepVars)
+        self.bse = pd.Series()
+        for i in range(len(self.indepVars)):
+            self.bse[self.indepVars[i]] = sqrt(self.cov_params[i,i])
+        self.tvalues = self.params/self.bse
+        _df = np.ones(len(self.indepVars))*(self.pooledDF.shape[0] - len(self.indepVars) - 2)
+        self.pvalues = sp.stats.tvalues.sf(self.tvalues, _df)*2
+        self.fittedvalues = self.pooledDF * self.params
+        self.resid = self.pooledDF[self.depVar] - self.fittedvalues
+        self.mse = np.sum(self.resid**2)/(self.pooledDF.shape[0] - len(self.indepVars) - 2)
+        self.rmse = sqrt(self.mse)
+        self.ssr = np.sum(self.resid**2)
+        _yBar = np.mean(self.pooledDF[self.depVar])
+        self.sst = np.sum(self.pooledDF[self.depVar] - _yBar)
+        self.rsquared = 1 - self.ssr/self.sst
+
         if self.robust:
             tempB = np.matrix(np.zeros((len(self.indepVars), len(self.indepVars))))
             for id in self.idVals:
                 tempData = np.matrix(self.panel.major_xs(id)[self.indepVars].as_matrix())
                 tempY = np.matrix(self.panel.major_xs(id)[self.depVar].as_matrix())
-                tempResid = tempY - tempData*self.betas
+                tempResid = tempY - tempData*self.params
                 tempB += tempData.T * self.omega.inv * tempResid * tempResid.T * self.omega.inv * tempData
-            self.cov_params_robust = (tempA.inv * tempB * tempA.inv) / len(self.idVals)
+            _cov_params_robust = (tempA.inv * tempB * tempA.inv) / len(self.idVals)
+            self.cov_params_robust = pd.DataFrame(_cov_params_robust, self.indepVars, self.indepVars)
+            for i in range(len(self.indepVars)):
+                self.bse[self.indepVars[i]] = sqrt(self.cov_params_robust[i,i])
+            self.tvalues = self.params/self.bse
+            self.pvalues = sp.stats.tvalues.sf(self.tvalues, _df)*2
 
-        #TODO: put the fitted values in for the parameters
+        _ciLowerBound = self.params - 1.96*self.bse
+        _ciUpperBound = self.params + 1.96*self.bse
+        self.conf_int = pd.merge(_ciLowerBound.to_frame(), _ciUpperBound.to_frame(), left_index=True, right_index=True)
+
 
     def summary(self):
-        pass
-        #TODO: Fill in this method
+        print """
 
-    def predict(self):
-        pass
-        #TODO: Fill in this method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        """
